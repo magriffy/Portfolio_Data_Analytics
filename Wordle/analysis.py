e02164cb1123560a653d4191a83a7f2fa9a02b91
Merpster456 <ryan.quirk6@protonmail.ch>
2024-11-13 15:04:29 -0500

    Added headers

--- Wordle/analysis.py
+++ Wordle/analysis.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from datetime import datetime
 
-df = pd.read_excel("Problem_C_Data_Wordle.xlsx", usecols="B,M")
+df = pd.read_excel("Problem_C_Data_Wordle.xlsx", usecols="B:M")
+# Date, Contest #, Word, # of reported results, # in hard mode, 1 try, 2 tries, 3 tries..
+df.columns = ["date", "contest", "word", "reported", "hard", "1", "2", "3", "4", "5", "6", "X"]
 
-df.head()
+print(df.head())

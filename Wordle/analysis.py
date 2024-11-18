457ef58cb9dca996a4849de3976129c4a02d13e1
Merpster456 <ryan.quirk6@protonmail.ch>
2024-11-18 11:46:41 -0500

    Added average of reports off word difficulty

--- Wordle/analysis.py
+++ Wordle/analysis.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestRegressor
 
 df = pd.read_excel("Problem_C_Data_Wordle.xlsx", usecols="B:M")
+
 # Date, Contest #, Word, # of reported results, # in hard mode, 1 try, 2 tries, 3 tries..
 df.columns = ["date", "contest", "word", "reported", "hard", "1", "2", "3", "4", "5", "6", "X"]
 
@@ -19,7 +20,6 @@ reported_by_day_hard = df.groupby('day_of_week')['hard'].mean()
 
 print(reported_by_day)
 print(reported_by_day_hard)
-print(df)
 
 # Order the days of the week correctly
 ordered_days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
@@ -32,7 +32,7 @@ reported_by_day.plot(kind='bar', color='skyblue')
 # reported_by_day_hard.plot(kind='bar', color='blue')
 plt.title("Number of Reported Results by Day of the Week")
 plt.xticks(rotation=45)
-plt.show()
+#plt.show()
 
 
 # Prepare lagged features
@@ -56,3 +56,75 @@ future_values = X[-1:]  # Taking the latest feature row for prediction
 predicted_value_rf = model_rf.predict(future_values)
 
 print(f"Predicted reported results on March 1, 2023 (Random Forest): {predicted_value_rf[0]}")
+
+
+
+
+
+"""
+Try to establish variation in data from number of consonants/vowels in word
+"""
+vowels = ["a", "e", "i", "o", "u"]
+
+i = 1
+
+
+
+def c_count(word):
+    """
+    Utility function that returns number of consonants in a word
+
+    :param word: Wordle word
+    :return: Number of consonants in word
+    """
+
+    consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y",
+                  "z"]
+    cs = 0
+    for l in word:
+        if l in consonants:
+            cs += 1
+    return cs
+
+c_list = []
+
+for word in df["word"]:
+    c_list.append(c_count(word))
+
+# Add number of consonants in word as column
+df["consonants"] = c_list
+
+reported_by_consonants = df.groupby('consonants')['reported'].mean()
+reported_by_consonants_hard = df.groupby('consonants')['hard'].mean()
+print(reported_by_consonants)
+print(reported_by_consonants_hard)
+
+plt.figure(figsize=(10, 6))
+reported_by_consonants_hard.plot(kind='bar', color='skyblue')
+# reported_by_day_hard.plot(kind='bar', color='blue')
+plt.title("Number of Reported Results by Number of Consonants in Word")
+plt.xticks(rotation=45)
+plt.show()
+
+easy = []
+hard = []
+e_perc = (df["reported"], df["2"] + df["3"] + df["4"])
+h_perc = (df["reported"], df["5"] + df["6"] + df["X"])
+
+h_avg = 0
+
+for index, row in df.iterrows():
+
+    e_perc = row["2"] + row["3"] + row["4"]
+    h_perc = row["5"] + row["6"] + row["X"]
+
+    if e_perc >= 50:
+        easy.append(row["hard"])
+    if h_perc >= 50:
+        hard.append(row["hard"])
+
+h_avg = sum(hard) / len(hard)
+e_avg = sum(easy) / len(easy)
+
+print(f"Easy: {e_avg}")
+print(f"Hard: {h_avg}")

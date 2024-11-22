111406f02246cd84df4929670e7b892382f018c1
Merpster456 <ryan.quirk6@protonmail.ch>
2024-11-22 13:11:45 -0500

    Added subplots to ipynb

--- GoldvsBitCoin/analysis.py
+++ GoldvsBitCoin/analysis.py
@@ -15,11 +15,8 @@ for date in bc_df["Date"]:
 
 print(len(gold_df["Date"]))
 b_xtcks = [bc_df["Date"][0], bc_df["Date"][365], bc_df["Date"][730], bc_df["Date"][1095], bc_df["Date"][1460], bc_df["Date"][1825]]
-
 g_xtcks = [gold_df["Date"][0], gold_df["Date"][253], gold_df["Date"][506], gold_df["Date"][759], gold_df["Date"][1012], gold_df["Date"][1264]]
 
-for date in gold_df["Date"]:
-    print(bc_df[])
 
 for i in range(100):
     print(f"GOLD: {gold_df["Date"][i]}\nBTC: {bc_df["Date"][i]}\n")
@@ -29,13 +26,13 @@ figure.set_figwidth(12)
 figure.set_figheight(10)
 
 # Bitcoin plot
-axis[0].plot(bc_df["Date"], bc_df["Value"])
+axis[0].plot(bc_df["Date"], bc_df["Value"], color='b')
 axis[0].set_xticks(b_xtcks)
 axis[0].set_xlabel("Date")
 axis[0].set_ylabel("Price of 1 Bitcoin")
 
 # Gold plot
-axis[1].plot(gold_df["Date"], gold_df["Value"])
+axis[1].plot(gold_df["Date"], gold_df["Value1"], color='r')
 axis[1].set_xticks(g_xtcks)
 axis[1].set_xlabel("Date")
 axis[1].set_ylabel("Price of 1 Troy Oz. of Gold")
